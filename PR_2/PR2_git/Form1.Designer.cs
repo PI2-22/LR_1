0f16976a59da462eb862fb720c81e30b9de3fabf
PI2-22 <nickita.shebalkov@yandex.ru>
2024-03-18 23:32:52 +0300

    добавлен вывод сообщения в дополнительной ветке

--- PR_2/PR2_git/Form1.Designer.cs
+++ PR_2/PR2_git/Form1.Designer.cs
@@ -29,30 +29,58 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(325, 121);
+            button1.Location = new Point(58, 90);
             button1.Name = "button1";
             button1.Size = new Size(306, 166);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(443, 145);
+            button2.Name = "button2";
+            button2.Size = new Size(261, 143);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(301, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 25);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private Button button2;
+        private Label label1;
     }
 }
--- PR_2/PR2_git/Form1.cs
+++ PR_2/PR2_git/Form1.cs
@@ -6,5 +6,21 @@ namespace PR2_git
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+         
+            label1.Text = "Это выполнено в ветке Ivanov_Sergey";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
